7f3b5b2c26df82114b1c10aa9a6336bb135606b8
Ooo0ption <1271561104@qq.com>
2024-09-11 01:35:54 -0400

    fix the app compile

--- TestFreeAuth/ApplicationDemo/compile_backend.sh
+++ TestFreeAuth/ApplicationDemo/compile_backend.sh
@@ -2,7 +2,7 @@
 set -e
 project_root="$(dirname $0)/../../"
 [ ! -d $project_root/build ] && mkdir $project_root/build
-[ ! -d $project_root/FreeAuth/ApplicationDemo/libs ] && mkdir $project_root/TestFreeAuth/ApplicationDemo/libs
+[ ! -d $project_root/TestFreeAuth/ApplicationDemo/libs ] && mkdir $project_root/TestFreeAuth/ApplicationDemo/libs
 [ ! -f $project_root/build/Makefile ] && cmake -S $project_root -B $project_root/build
 make -j6 -C $project_root/build
 cp $project_root/build/libNodeWrapperSMTP.* $project_root/TestFreeAuth/ApplicationDemo/libs/
