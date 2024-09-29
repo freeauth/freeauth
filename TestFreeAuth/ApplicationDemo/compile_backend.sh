36ba8cc8417ba2884a940a4664bf8d0ce3df5d8e
Ooo0ption <1271561104@qq.com>
2024-09-29 07:32:39 -0400

    fix the compile_backend.sh

--- TestFreeAuth/ApplicationDemo/compile_backend.sh
+++ TestFreeAuth/ApplicationDemo/compile_backend.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 set -e
-project_root="$(dirname $0)/../../"
+project_root="$(dirname $0)"
+project_root="$(realpath $project_root)"
+project_root="$(dirname $project_root)"
+project_root="$(dirname $project_root)"
+echo "$project_root"
 [ ! -d $project_root/build ] && mkdir $project_root/build
 [ ! -d $project_root/TestFreeAuth/ApplicationDemo/libs ] && mkdir $project_root/TestFreeAuth/ApplicationDemo/libs
 [ ! -f $project_root/build/Makefile ] && cmake -S $project_root -B $project_root/build
